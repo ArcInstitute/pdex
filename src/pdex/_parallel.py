fix: skip calculation if μ_ref is zero

src/pdex/_parallel.py
@@ -160,10 +160,9 @@ def _fold_change(
     μ_ref: float,
 ) -> float:
     """Calculate the fold change between two means."""
-    try:
-        return μ_tgt / μ_ref
-    except ZeroDivisionError:
+    if μ_ref == 0:
         return np.nan
+    return μ_tgt / μ_ref
 
 
 def _percent_change(
@@ -171,10 +170,9 @@ def _percent_change(
     μ_ref: float,
 ) -> float:
     """Calculate the percent change between two means."""
-    try:
-        return (μ_tgt - μ_ref) / μ_ref
-    except ZeroDivisionError:
+    if μ_ref == 0:
         return np.nan
+    return (μ_tgt - μ_ref) / μ_ref
 
 
 def parallel_differential_expression(