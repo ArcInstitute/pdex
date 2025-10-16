b2974b2574b8b6f430bc46d51f447ec2efe4213a
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2025-10-16 09:53:19 -0700

    refactor: check if log using fractional component

--- src/pdex/_utils.py
+++ src/pdex/_utils.py
@@ -1,5 +1,6 @@
 import anndata as ad
 import numpy as np
+from scipy.sparse import csc_matrix, csr_matrix
 
 # A heuristic to determine if the data is log-transformed
 # Checks if the mean cell umi count is greater than a certain threshold
@@ -9,24 +10,22 @@ import numpy as np
 # average UMI count of ($ e^{15} - 1 = 3.26M $ ) which is unlikely at this point)
 UPPER_LIMIT_LOG = 15
 
+EPSILON = 1e-6
+
 
 def guess_is_log(adata: ad.AnnData, num_cells: int | float = 5e2) -> bool:
-    """Make an *educated* guess whether the provided anndata is log-transformed.
-
-    Selects a random subset of cells and sums their counts.
-    Returns false if all decimal components are zero (unlikely for log transformed data)
     """
-    # Select either the provided `num_cells` or the maximum number of cells in the `adata`
-    num_cells = int(min(num_cells, adata.shape[0]))
+    Make an *educated* guess whether the provided anndata is log-transformed.
 
-    # Draw a random mask of cells
-    mask = np.random.choice(adata.shape[0], size=num_cells, replace=False)
+    Checks whether the any fractional value of the matrix is greater than an epsilon.
 
-    # Sum the matrix across the selected cell subset
-    sums = adata[mask].X.sum(axis=1)  # type: ignore
+    This *cannot* tell the difference between log and normalized data.
+    """
+    if isinstance(adata.X, csr_matrix) or isinstance(adata.X, csc_matrix):
+        frac, _ = np.modf(adata.X.data)
+    elif adata.X is None:
+        raise ValueError("adata.X is None")
+    else:
+        frac, _ = np.modf(adata.X) # type: ignore
 
-    # Determine the mean cell umi count
-    mean_umi_count = np.mean(sums)
-
-    # Return True if the mean cell umi count is less than the upper limit
-    return bool(mean_umi_count < UPPER_LIMIT_LOG)
+    return bool(np.any(frac > EPSILON))
