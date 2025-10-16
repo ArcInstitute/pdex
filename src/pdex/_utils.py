77900307077890248e7f2cafd6509886a8ff38de
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2025-10-16 10:07:38 -0700

    chore: remove unused argument for guess_is_log

--- src/pdex/_utils.py
+++ src/pdex/_utils.py
@@ -2,18 +2,10 @@ import anndata as ad
 import numpy as np
 from scipy.sparse import csc_matrix, csr_matrix
 
-# A heuristic to determine if the data is log-transformed
-# Checks if the mean cell umi count is greater than a certain threshold
-# If the the mean cell umi count is < UPPER_LIMIT_LOG, it is assumed that the data is log-transformed
-#
-# This limit is set to 15 (log-data with >15 average UMI counts would mean an
-# average UMI count of ($ e^{15} - 1 = 3.26M $ ) which is unlikely at this point)
-UPPER_LIMIT_LOG = 15
-
 EPSILON = 1e-3
 
 
-def guess_is_log(adata: ad.AnnData, num_cells: int | float = 5e2) -> bool:
+def guess_is_log(adata: ad.AnnData) -> bool:
     """
     Make an *educated* guess whether the provided anndata is log-transformed.
 
