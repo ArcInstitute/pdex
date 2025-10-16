4fa5567bda02e0336da38b7b04b603c35178fb8a
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2025-10-16 09:53:38 -0700

    tests: added more testing on log check

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,6 @@
 import anndata as ad
 import numpy as np
+from scipy.sparse import csc_matrix, csr_matrix
 
 from pdex._utils import guess_is_log
 
@@ -8,18 +9,36 @@ N_GENES = 10
 MAX_COUNT = 1e6
 
 
-def build_anndata(log=False) -> ad.AnnData:
+def build_anndata(log=False, sparse : str | None = None) -> ad.AnnData:
     dim = (N_CELLS, N_GENES)
+    matrix = np.random.random(size=dim)
+    if sparse == "csr":
+        matrix = csr_matrix(matrix)
+    elif sparse == "csc":
+        matrix = csc_matrix(matrix)
     return ad.AnnData(
-        X=np.random.random(size=dim)
+        X=matrix
         if log
         else np.random.randint(0, int(MAX_COUNT), size=dim)
     )
 
 
-def test_log_guess():
-    log_anndata = build_anndata(log=True)
-    assert guess_is_log(log_anndata)
+def test_log_guess_logtrue():
+    adata = build_anndata(log=True)
+    assert guess_is_log(adata)
 
-    count_anndata = build_anndata(log=False)
-    assert not guess_is_log(count_anndata)
+    adata = build_anndata(log=True, sparse="csc")
+    assert guess_is_log(adata)
+
+    adata = build_anndata(log=True, sparse="csr")
+    assert guess_is_log(adata)
+
+def test_log_guess_logfalse():
+    adata = build_anndata(log=False)
+    assert not guess_is_log(adata)
+
+    adata = build_anndata(log=False, sparse="csc")
+    assert not guess_is_log(adata)
+
+    adata = build_anndata(log=False, sparse="csr")
+    assert not guess_is_log(adata)
