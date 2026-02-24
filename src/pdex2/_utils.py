bf92d177bed9c07a7384c55317eb00f82ec4debc
Noam Teyssier <22600644+noamteyssier@users.noreply.github.com>
2026-02-24 14:53:42 -0800

    fix: edge cases in threads

--- src/pdex2/_utils.py
+++ src/pdex2/_utils.py
@@ -4,11 +4,16 @@ import numba
 
 
 def set_numba_threadpool(threads: int = 0):
+    available_threads = os.cpu_count()
+    if available_threads is None:
+        available_threads = 1
+
     if threads == 0:
-        available_threads = os.cpu_count()
         if not available_threads:
             threads = 1
         else:
             threads = available_threads
+    else:
+        threads = min(threads, available_threads)
 
     numba.config.NUMBA_NUM_THREADS = threads  # type: ignore
